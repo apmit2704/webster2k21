81089b94ee5080cc1d37128e0d528bec4f0f7d0a
khushi <lestrangebellatrix11@gmail.com>
2021-10-14 16:50:36 +0530

    auth

--- BerlinCheckers/BerlinCheckers/urls.py
+++ BerlinCheckers/BerlinCheckers/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('CheckersGame.urls')),
+    path('', include('accounts.urls')),
 ]
