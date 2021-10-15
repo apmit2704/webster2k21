61f8f8c7e983e51f756d727d07a7b8c712815d47
Shreya Yadav <shreyayadav987@gmail.com>
2021-10-16 00:01:21 +0530

    Game created

--- BerlinCheckers/CheckersGame/views.py
+++ BerlinCheckers/CheckersGame/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 # Create your views here.
 
-def register(request):
-    pass
-def login(request):
-    pass
 
 def home(request):
-    return render(request, 'templates/profile.html')
+    return render(request, 'profile.html')
 
 def play(request, room_code):
-    return render(request, 'templates/play.html')
+    # user = User.objects.get(id = request.user.id)
+    # game = Game.objects.get(room_code = room_code)
+    # if game.game_creator != user and game.game_opponent != user:
+    #     return redirect('/')
+    # if game.is_over:
+    #     return redirect('/')
+    context = {
+        'username' : request.GET['username'],
+        'room_code' : room_code
+    }
+    return render(request, 'play.html', context)
