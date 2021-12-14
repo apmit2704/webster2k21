e2e4346e47ffaa3d33ad4b284b130b7e53c70626
khushi <lestrangebellatrix11@gmail.com>
2021-12-14 17:08:21 +0530

    profile

--- BerlinCheckers/CheckersGame/models.py
+++ BerlinCheckers/CheckersGame/models.py
@@ -36,16 +36,6 @@ class BoardSquare(models.Model):
     ])
     game = models.ForeignKey(Game, on_delete=CASCADE)
 
-class Player(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    profile_pic = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
 
 admin.site.register(Game)
 admin.site.register(BoardSquare)
-admin.site.register(Player)
