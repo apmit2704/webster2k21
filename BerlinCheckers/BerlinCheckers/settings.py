764c953ce7e541793f92d8cb8745a8169165e227
hackko-20 <60813236+hackko-20@users.noreply.github.com>
2021-12-26 03:07:24 +0530

    Update settings.py

--- BerlinCheckers/BerlinCheckers/settings.py
+++ BerlinCheckers/BerlinCheckers/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER' : 'postgres',
-        'PASSWORD' : 'imagoogler',
+        'PASSWORD' : 'password7',
         'HOST': 'localhost',
         'PORT': '5432',
     }
