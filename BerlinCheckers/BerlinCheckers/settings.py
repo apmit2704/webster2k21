2b59b17726e98e0049fa750f71b0c2e7ab1e6f22
Shreya Yadav <shreyayadav987@gmail.com>
2021-12-27 17:33:18 +0530

    latest version

--- BerlinCheckers/BerlinCheckers/settings.py
+++ BerlinCheckers/BerlinCheckers/settings.py
@@ -113,9 +113,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER' : 'postgres',
-        'PASSWORD' : 'imagoogler',
+        'PASSWORD' : 'pasword7',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '5433',
     }
     
 }
